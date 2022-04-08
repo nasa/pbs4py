e442ade3e34884b3564c28be3cd5a73cb116ba96
Kevin Jacobson <kevin.e.jacobson@nasa.gov>
2022-04-08 08:18:22 -0400

    Better documentation of job_dir script

--- pbs4py/scripts/job_dir.py
+++ pbs4py/scripts/job_dir.py
@@ -1,15 +1,6 @@
 #!/usr/bin/env python
 """
 A script to print the directory that a job is running in.
-
-Can be used in combination with bash aliases to create a `qdir` alias for moving
-to the directory a job is running in
-```
-qdirfun() { cd `job_dir.py $1`;}
-alias qdir=qdirfun
-```
-
-Then in the shell instance you can do `qdir {job_id}` to move the job's run directory.
 """
 import argparse
 from pbs4py.job import PBSJob
