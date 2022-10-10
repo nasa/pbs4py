88074355f5b8760c450187f4c4b0eab699157dc0
Kevin Jacobson <kevin.e.jacobson@nasa.gov>
2022-10-10 14:54:33 -0400

    Changes to tests to run on without relying on a ~/.bashrc to exist

--- tests/test_bsub_regression.py
+++ tests/test_bsub_regression.py
@@ -3,6 +3,22 @@ import filecmp
 from pbs4py.bsub import BSUB
 
 test_directory = os.path.dirname(os.path.abspath(__file__))
+test_profile = f'{test_directory}/testing_bashrc'
+
+
+def modify_golden_file_to_have_right_path_for_profile(golden_file: str, profile_filename: str):
+    with open(golden_file, 'r') as fh:
+        golden_file_contents = fh.readlines()
+    golden_file_with_profile = []
+    for line in golden_file_contents:
+        if line == 'source_line\n':
+            golden_file_with_profile.append(f'source {profile_filename}\n')
+        else:
+            golden_file_with_profile.append(line)
+    golden_mod = f'{test_directory}/test_output_files/golden_mod.lsf'
+    with open(golden_mod, 'w') as fh:
+        fh.writelines(golden_file_with_profile)
+    return golden_mod
 
 
 def test_write_job_file_regression_check():
@@ -10,9 +26,9 @@ def test_write_job_file_regression_check():
     project = 'ard149'
     ngpu = 5
     time = 24
-    hashbang = '#!/usr/bin/tcsh'
-    bsub = BSUB(project, time=time)
-    bsub.hashbang = hashbang
+    shell = 'tcsh'
+    bsub = BSUB(project, time=time, profile_filename=test_profile)
+    bsub.shell = shell
     bsub.requested_number_of_nodes = 2
 
     job_name = 'test_job'
@@ -20,7 +36,10 @@ def test_write_job_file_regression_check():
     bsub_file = f'{test_directory}/test_output_files/test.lsf'
     bsub.write_job_file(bsub_file, job_name, job_body)
 
-    assert filecmp.cmp(bsub_file, golden_file)
+    golden_mod = modify_golden_file_to_have_right_path_for_profile(
+        golden_file, bsub.profile_filename)
+
+    assert filecmp.cmp(bsub_file, golden_mod)
 
 
 if __name__ == '__main__':
--- tests/test_pbs_regression.py
+++ tests/test_pbs_regression.py
@@ -1,9 +1,24 @@
 import os
 import filecmp
-from typing import List
 from pbs4py import PBS
 
 test_directory = os.path.dirname(os.path.abspath(__file__))
+test_profile = f'{test_directory}/testing_bashrc'
+
+
+def modify_golden_file_to_have_right_path_for_profile(golden_file: str, profile_filename: str):
+    with open(golden_file, 'r') as fh:
+        golden_file_contents = fh.readlines()
+    golden_file_with_profile = []
+    for line in golden_file_contents:
+        if line == 'source_line\n':
+            golden_file_with_profile.append(f'source {profile_filename}\n')
+        else:
+            golden_file_with_profile.append(line)
+    golden_mod = f'{test_directory}/test_output_files/golden_mod.pbs'
+    with open(golden_mod, 'w') as fh:
+        fh.writelines(golden_file_with_profile)
+    return golden_mod
 
 
 def test_write_job_file_regression_check():
@@ -14,7 +29,8 @@ def test_write_job_file_regression_check():
     time = 24
     hashbang = '#!/usr/bin/bash'
     pbs = PBS(queue_name=queue_name, ncpus_per_node=ncpus_per_node,
-              queue_node_limit=queue_node_limit, time=time)
+              queue_node_limit=queue_node_limit, time=time,
+              profile_file=test_profile)
     pbs.hashbang = hashbang
 
     job_name = 'test_job'
@@ -22,7 +38,10 @@ def test_write_job_file_regression_check():
     pbs_file = f'{test_directory}/test_output_files/test.pbs'
     pbs.write_job_file(pbs_file, job_name, job_body)
 
-    assert filecmp.cmp(pbs_file, golden_file)
+    golden_mod = modify_golden_file_to_have_right_path_for_profile(
+        golden_file, pbs.profile_filename)
+
+    assert filecmp.cmp(pbs_file, golden_mod)
 
 
 if __name__ == '__main__':
