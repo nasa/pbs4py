3b3ce28ba356d600047d9fda05c066e004bf54dc
Kevin Jacobson <kevin.e.jacobson@nasa.gov>
2024-10-24 15:33:39 -0400

    Bump to version 1.0.6

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ from setuptools import setup, find_packages
 
 
 __package_name__ = "pbs4py"
-__package_version__ = "1.0.5"
+__package_version__ = "1.0.6"
 
 root = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(root, "README.md"), encoding="utf-8") as f:
